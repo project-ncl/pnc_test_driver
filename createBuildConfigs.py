b932283c568a74ef44e5632fa7a6b0da2b526e48
Mansi Vaghela <mvaghela@redhat.com>
2015-09-23 17:24:09 -0400

    return build config ids

--- createBuildConfigs.py
+++ createBuildConfigs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import json
 import requests
+
 SERVER_NAME = "http://localhost:8080"
 
 with open('sampleBuildConfigs/dependantProjects.json') as f:
@@ -8,9 +9,7 @@ with open('sampleBuildConfigs/dependantProjects.json') as f:
     for line in f:
         headers = {'content-type': 'application/json'}
         r = requests.post(SERVER_NAME + "/pnc-rest/rest/build-configurations/", data=line, headers=headers)
-        print r.status_code
         print r.content
-        # data = json.loads(r.content)
-        # bc_ids.append(data["id"])
-        print line
+        data = json.loads(r.content)
+        bc_ids.append(data["id"])
     print bc_ids
