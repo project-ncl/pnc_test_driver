918cb2cbb645927ecd8b2b58596f4799bc3003ea
Emmett Underhill <eunderhi@redhat.com>
2015-09-25 12:07:32 -0400

    Getting build times now
    New functions to get the build time for all build records

--- createBuildConfigs.py
+++ createBuildConfigs.py
@@ -8,6 +8,7 @@ from time import sleep
 SERVER_NAME = "http://localhost:8080"
 buildConfigIds = []
 recordIds = []
+buildTimes = []
 
 def getId(data):
     contentKey = unicode("content", "utf-8")
@@ -37,6 +38,21 @@ def buildsAreRunning():
             return True
     return False
 
+def getAllBuildTimes():
+    for i in recordIds:
+        buildTimes.append(getTime(i))
+
+def getTime(buildId):
+    r = requests.get(SERVER_NAME + "/pnc-rest/rest/build-records/" + str(buildId))
+    content = json.loads(r.content)
+
+    contentKey = unicode("content", "utf-8")
+    startTimeKey = unicode("startTime", "utf-8")
+    endTimeKey = unicode("endTime", "utf-8")
+
+    time = int(content[contentKey][startTimeKey]) - int(content[contentKey][startTimeKey])
+    return time
+
 def randomName(size=6, chars=string.ascii_uppercase + string.digits + string.ascii_lowercase):
    return ''.join(random.choice(chars) for i in range(size))
 
@@ -53,3 +69,5 @@ with open('sampleBuildConfigs/dependantProjects.json') as f:
 
 fireBuilds(buildConfigIds)
 waitTillBuildsAreDone()
+getAllBuildTimes()
+print(buildTimes)
